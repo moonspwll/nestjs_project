211d5a462c0865ef089290a424596e89fe08dcbe
moonspwll <alexey.kowalskiy@gmail.com>
2025-03-06 18:33:57 +0200

    Add ValidationPipe for UserDto / Add user exists validation to userService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, Injectable, HttpStatus } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { sign } from "jsonwebtoken";
@@ -12,6 +12,13 @@ export class UserService {
     constructor(@InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>) {}
 
     async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+        const userByEmail = await this.userRepository.findOne({ where: { email: createUserDto.email }});
+        const userByUsername = await this.userRepository.findOne({ where: { username: createUserDto.username }});
+
+        if (userByEmail || userByUsername) {
+            throw new HttpException('Email or username is already taken', HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
         const newUser = new UserEntity();
         Object.assign(newUser, createUserDto);
         return this.userRepository.save(newUser);
